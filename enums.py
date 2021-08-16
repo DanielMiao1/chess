2c9e1802466b3e9fe30f62df736fcbe53a47e4bc
Daniel M <danielmiao2019@icloud.com>
2021-08-16 15:17:38 -0700

    added squares, pieces, and enumerated types

---
+++ enums.py
@@ -0,0 +1,16 @@
+"""
+enums.py
+Type Enumerations
+"""
+
+
+class Color:
+	white, black = 0, 1
+
+
+class Piece:
+	pawn, knight, bishop, rook, queen, king = 0, 1, 2, 3, 4, 5
+
+
+class Square:
+	light, dark = 0, 1
