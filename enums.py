a27360cdec5f0a5e3988726b2450d938d47fbe3c
Daniel M <danielmiao2019@icloud.com>
2021-09-04 10:16:41 -0700

    added setup file

--- enums.py
+++ enums.py
@@ -11,7 +11,3 @@ class Color:
 class Piece:
 	pawn, knight, bishop, rook = ["pawn", 1], ["knight", 3], ["bishop", 3], ["rook", 5]
 	queen, king = ["queen", 9], ["king", float("inf")]
-
-
-class Square:
-	light, dark = "light", "dark"
---
+++ setup.py
@@ -0,0 +1,15 @@
+"""
+setup.py
+Setup
+"""
+
+from distutils.core import setup
+
+setup(
+	name="ChessLibrary",
+	version="0.0.1",
+	author="Daniel Miao",
+	author_email="danielmiao2019@icloud.com",
+	url="https://github.com/DanielMiao1/ChessLibrary",
+	py_modules=["ChessLibrary", "functions", "errors", "enums"]
+)
