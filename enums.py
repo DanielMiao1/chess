b13b382aada06b1cc401ad6ed08ca9792c439e6c
Daniel M <danielmiao2019@icloud.com>
2021-09-05 18:27:52 -0700

    added show_data in moves function

--- enums.py
+++ enums.py
@@ -11,3 +11,10 @@ class Color:
 class Piece:
 	pawn, knight, bishop, rook = ["pawn", 1], ["knight", 3], ["bishop", 3], ["rook", 5]
 	queen, king = ["queen", 9], ["king", float("inf")]
+
+
+class Move:
+	def __init__(self, name, old_position, new_position, is_capture=False):
+		self.name = name
+		self.old_position, self.new_position = old_position, new_position
+		self.is_capture = is_capture
