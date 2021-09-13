fec7e2275ae01f98b31b852d55e2b56af85fea79
Daniel M <danielmiao2019@icloud.com>
2021-09-13 11:41:30 -0700

    updated move function

--- enums.py
+++ enums.py
@@ -14,7 +14,8 @@ class Piece:
 
 
 class Move:
-	def __init__(self, name, old_position, new_position, is_capture=False):
+	def __init__(self, name, old_position, new_position, piece, is_capture=False):
+		self.piece = piece
 		self.name = name
 		self.old_position, self.new_position = old_position, new_position
 		self.is_capture = is_capture
