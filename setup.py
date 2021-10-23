681509d1084dcbc342df62cb04c7f7132aec2997
Daniel M <danielmiao2019@icloud.com>
2021-10-22 19:18:55 -0700

    added a queen moves generator

--- setup.py
+++ setup.py
@@ -5,14 +5,13 @@ Setup
 
 import setuptools
 import sys
-import os
 
 if sys.version_info < (2, 7):
 	raise ImportError("This library requires Python 2.7 or later.")
 
 setuptools.setup(
 	name="chess",
-	version="v0.2.2-dev6",
+	version="v0.2.3-dev",
 	author="Daniel M",
 	author_email="danielmiao2019@icloud.com",
 	description="Chess Library for Python 3 and Python 2",
