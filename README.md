5cc44a07d11e22668209ac0af0228a5ddff3232a
Daniel M <danielmiao2019@icloud.com>
2022-02-08 16:19:56 -0800

    updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# ChessLibrary
-**Chess Library in Python 3**
+# chess
+**A python chess library with legal move generation, custom chess variant support, move making and unmaking, PGN/FEN loading/generating, and many other features**
+## Requirements
+Python 2.7 or Python 3
+## Installation
+The library can be installed directly from github using pip: `pip install git+https://github.com/DanielMiao1/chess`.
+## Basic Features
+**See the features section of the wiki for a list of features**
+
+### Creating a new game
+```py
+>>> game = chess.Game()
+```
+To load a custom FEN, use the fen argument:
+```py
+>>> game = chess.Game(fen="fen text")
+```
+Or, use the `loadFEN` function to load a FEN after the game is initialized:
+```py
+>>> game.loadFEN("fen text")
+```
+### Printing the board
+To generate a unicode representation of the board, use the `Game.visualized()` function.
+### Making a move
+To make a move, use the `Game.move()` function:
+```py
+>>> game.move("e4")
+e4
+### Takeback
+To take back a move, use the `Game.takeback()` function:
+```py
+>>> game.takeback()
+```
--- minified_chess.py
+++ minified_chess.py
@@ -3425,60 +3425,60 @@ class openings:
 class errors:
 	class Error(Exception):
 		def __init__(self, message):
-			super(Error, self).__init__(message)
+			super(errors.Error, self).__init__(message)
 	
 	class MoveNotPossible(Error):
 		def __init__(self, move):
-			super(MoveNotPossible, self).__init__("Move '" + str(move) + "' is not possible")
+			super(errors.MoveNotPossible, self).__init__("Move '" + str(move) + "' is not possible")
 	
 	class InvalidMove(Error):
 		def __init__(self, move):
-			super(InvalidMove, self).__init__("Move '" + str(move) + "' is invalid")
+			super(errors.InvalidMove, self).__init__("Move '" + str(move) + "' is invalid")
 	
 	class InvalidFEN(Error):
 		def __init__(self, fen):
-			super(InvalidFEN, self).__init__("FEN '" + str(fen) + "' is invalid")
+			super(errors.InvalidFEN, self).__init__("FEN '" + str(fen) + "' is invalid")
 	
 	class InvalidCoordinate(Error):
 		def __init__(self, coordinate):
-			super(InvalidCoordinate, self).__init__("Coordinate '" + str(coordinate) + "' is invalid")
+			super(errors.InvalidCoordinate, self).__init__("Coordinate '" + str(coordinate) + "' is invalid")
 	
 	class InvalidColor(Error):
 		def __init__(self, message):
-			super(InvalidColor, self).__init__(message)
+			super(errors.InvalidColor, self).__init__(message)
 	
 	class InvalidPiece(Error):
 		def __init__(self, piece):
-			super(InvalidPiece, self).__init__("Piece '" + str(piece) + "' is invalid")
+			super(errors.InvalidPiece, self).__init__("Piece '" + str(piece) + "' is invalid")
 	
 	class UndefinedColor(Error):
 		def __init__(self, color):
 			if color.lower() == "w":
-				super(UndefinedColor, self).__init__("Color 'w' is invalid. Maybe you meant 'white'?")
+				super(errors.UndefinedColor, self).__init__("Color 'w' is invalid. Maybe you meant 'white'?")
 			elif color.lower() == "b":
-				super(UndefinedColor, self).__init__("Color 'b' is invalid. Maybe you meant 'black'?")
+				super(errors.UndefinedColor, self).__init__("Color 'b' is invalid. Maybe you meant 'black'?")
 			else:
-				super(UndefinedColor, self).__init__("Color '" + str(color) + "' is invalid")
+				super(errors.UndefinedColor, self).__init__("Color '" + str(color) + "' is invalid")
 	
 	class UndefinedPiece(Error):
 		def __init__(self, piece):
-			super(UndefinedPiece, self).__init__("Piece '" + str(piece) + "' is invalid")
+			super(errors.UndefinedPiece, self).__init__("Piece '" + str(piece) + "' is invalid")
 	
 	class UndefinedGamePhase(Error):
 		def __init__(self, phase):
-			super(UndefinedGamePhase, self).__init__("Game phase '" + str(phase) + "' is invalid")
+			super(errors.UndefinedGamePhase, self).__init__("Game phase '" + str(phase) + "' is invalid")
 	
 	class InvalidLineCoordinates(Error):
 		def __init__(self, start, end):
-			super(InvalidLineCoordinates, self).__init__("The starting position " + str(start) + " and ending position " + str(end) + " do not form a valid line")
+			super(errors.InvalidLineCoordinates, self).__init__("The starting position " + str(start) + " and ending position " + str(end) + " do not form a valid line")
 	
 	class InvalidPGNLine(Error):
 		def __init__(self, line, line_number):
-			super(InvalidPGNLine, self).__init__("Invalid PGN (line " + str(line_number) + "):\n  " + line)
+			super(errors.InvalidPGNLine, self).__init__("Invalid PGN (line " + str(line_number) + "):\n  " + line)
 	
 	class InvalidPGNMove(Error):
 		def __init__(self, move, move_number):
-			super(InvalidPGNMove, self).__init__("Invalid move in PGN:\n  " + str(move_number) + ". " + move)
+			super(errors.InvalidPGNMove, self).__init__("Invalid move in PGN:\n  " + str(move_number) + ". " + move)
 
 
 class functions:
